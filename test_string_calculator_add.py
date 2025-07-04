58199ce75290e3729f3f87a10c28d283bd91bac8
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2025-07-04 14:16:33 +0530

    [Task: 1] added cases for two or three numbers, failed for these cases

--- test_string_calculator_add.py
+++ test_string_calculator_add.py
@@ -13,3 +13,8 @@ def test_add_empty_string(string_calculator):
 def test_add_single_number(string_calculator):
     assert string_calculator.add("2") == 2
     
+    
+def test_add_two_or_three_numbers(string_calculator):
+    assert string_calculator.add("2,3") == 5
+    assert string_calculator.add("1,2,3") == 6
+    assert string_calculator.add("4,5,6") == 15
