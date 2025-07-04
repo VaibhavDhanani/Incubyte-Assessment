d3666cf69b2f0bac6cbae79c7e5a5df9372332d9
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2025-07-04 14:22:00 +0530

    [Task:1 ] refactored code, removed redundant if code

--- string_calculator.py
+++ string_calculator.py
@@ -5,13 +5,8 @@ class StringCalculator:
         if not numbers:
             return 0
         
-        num = [int(ele) for ele in numbers.split(',')]
+        num = 0
+        for i in numbers.split(","):
+            num += int(i)
         
-        if len(num) == 2:
-            return num[0] + num[1]
-        
-        if len(num) == 3:
-            return num[0] + num[1] + num[2]
-        
-        
-        return num[0]
+        return num
