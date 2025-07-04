f753455bf440c55ee4dcd4ef7ca64b07358cb306
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2025-07-04 14:25:26 +0530

    [Task: 2] add test cases for unknown number of inputs, passed these cases

--- test_string_calculator_add.py
+++ test_string_calculator_add.py
@@ -18,3 +18,9 @@ def test_add_two_or_three_numbers(string_calculator):
     assert string_calculator.add("2,3") == 5
     assert string_calculator.add("1,2,3") == 6
     assert string_calculator.add("4,5,6") == 15
+    
+    
+def test_add_unknown_numbers(string_calculator):
+    assert string_calculator.add("1,2,3,4,5,6,7,8,9") == 45
+    assert string_calculator.add("10,20,30,40,50,60,70,80,90,100") == 550
+    assert string_calculator.add("1,2,3,6,5,4") == 21
